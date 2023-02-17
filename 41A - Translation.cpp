6c5cab66fbaccd36e5c36a854bf04ab845626732
its-vikash <dev.vikashpatel@gmail.com>
2023-02-17 16:22:18 +0530

    Added 41A - Translation.cpp

---
+++ 41A - Translation.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Code Written By: Vikash Patel
+
+int main()
+{
+    string s,t;
+    cin>>s>>t;
+    reverse(s.begin(),s.end());
+    if(s==t)
+    cout<<"YES"<<endl;
+    else
+    cout<<"NO"<<endl;
+}
+    
+
+
+
+
+
+
