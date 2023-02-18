8fdbd031426b8335b1052d5e8af6b30ba7a3cf70
its-vikash <dev.vikashpatel@gmail.com>
2023-02-18 09:01:06 +0530

    Added 4A - Watermelon.cpp

---
+++ 4A - Watermelon.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Code Written By: Vikash Patel
+
+int main()
+{
+    int w;
+    cin>>w;
+    if(w%2==0 && w>2)
+    cout<<"YES"<<endl;
+    else
+    cout<<"NO"<<endl;
+}
