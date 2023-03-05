a0cb0ec015ecdd5a1eeb7e0eb7bb64087975b61f
its-vikash <dev.vikashpatel@gmail.com>
2023-03-05 12:48:51 +0530

    Added 1131A - Sea Battle.cpp

---
+++ 1131A - Sea Battle.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Code Written By: Vikash Patel
+
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+
+int main()
+{
+    int w1,h1,w2,h2;
+    cin>>w1>>h1>>w2>>h2;
+    cout<< ((h1+h2)+2)*2 + max(w1,w2)*2 <<endl; 
+}
