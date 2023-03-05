173c70b16568b9e413eb3fffd0718d1bbf9023c5
its-vikash <dev.vikashpatel@gmail.com>
2023-03-05 11:49:41 +0530

    Added 1081A - Definite Game.cpp

---
+++ 1081A - Definite Game.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Code Written By: Vikash Patel
+
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+
+int main()
+{
+    int v;
+    cin>>v;
+    if(v==2)
+    cout<<2<<endl;
+    else
+    cout<<1<<endl;
+}
