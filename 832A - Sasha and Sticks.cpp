8310b5e5413114671e2582d63fc230bc906c1489
its-vikash <dev.vikashpatel@gmail.com>
2023-03-05 21:36:45 +0530

    Added 832A - Sasha and Sticks.cpp

---
+++ 832A - Sasha and Sticks.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Code Written By: Vikash Patel
+
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+
+int main()
+{
+    long long n,k;
+    cin>>n>>k;
+    if((n/k) & 1)
+    cout<<"YES"<<endl;
+    else
+    cout<<"NO"<<endl;
+}
+
