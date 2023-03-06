620f3e03671a2ebd058e37cc565c08266e527193
its-vikash <dev.vikashpatel@gmail.com>
2023-03-06 19:51:16 +0530

    Added 1369A - FashionabLee.cpp

---
+++ 1369A - FashionabLee.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Code Written By: Vikash Patel
+
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        if(n%4==0)
+        cout<<"YES"<<endl;
+        else
+        cout<<"NO"<<endl;
+    }
+}
