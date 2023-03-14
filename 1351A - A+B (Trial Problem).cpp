f1134d5316059455aaf1ec25f236f754264d0993
its-vikash <dev.vikashpatel@gmail.com>
2023-03-14 12:28:16 +0530

    Added 1351A - A+B (Trial Problem).cpp

---
+++ 1351A - A+B (Trial Problem).cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Code Written By: Vikash Patel
+
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b;
+        cin>>a>>b;
+        cout<<a+b<<endl;
+    }
+}
