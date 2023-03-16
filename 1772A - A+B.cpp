3335bbe3651ba175768c41a0e256fbc31f144e4d
its-vikash <dev.vikashpatel@gmail.com>
2023-03-16 22:20:11 +0530

    Added 1772A - A+B?.cpp

---
+++ 1772A - A+B.cpp
@@ -0,0 +1,18 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        cout<<s[0]-'0'+s[2]-'0'<<endl;
+    }
+}
