bbe2d222073afeb8f73ab719f9afd1fdaad1d0fc
its-vikash <dev.vikashpatel@gmail.com>
2023-03-19 20:33:41 +0530

    Added 1807A - Plus or Minus.cpp

---
+++ 1807A - Plus or Minus.cpp
@@ -0,0 +1,21 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a+b==c)
+        cout<<"+"<<endl;
+        else
+        cout<<"-"<<endl;
+    }
+}
