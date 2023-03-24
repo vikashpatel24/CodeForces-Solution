b0227a8e751b99bdfefad37d8df4a8f62d9d9dea
its-vikash <dev.vikashpatel@gmail.com>
2023-03-24 23:12:50 +0530

    Added 266B - Queue at the School.cpp

---
+++ 266B - Queue at the School.cpp
@@ -0,0 +1,28 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+#define pb push_back
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+ 
+int main()
+{
+    int n, t;
+    string s;
+    cin >> n >> t >> s;
+    while (t--)
+    {
+        for (int i = 1; i < n; ++i)
+        {
+            if (s[i] == 'G' && s[i-1] == 'B')
+            {
+                s[i] = 'B';
+                s[i-1] = 'G';
+                ++i;
+            }
+        }
+    }
+    cout << s << endl;
+    return 0;
+}
