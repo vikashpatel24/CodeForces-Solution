8643b76f5ddf94e64b4c2406b547c658a49f8ca5
its-vikash <dev.vikashpatel@gmail.com>
2023-03-31 10:15:12 +0530

    Added 1335A - Candies and Two Sisters.cpp

---
+++ 1335A - Candies and Two Sisters.cpp
@@ -0,0 +1,21 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+#define pb push_back
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        double n;
+        cin>>n;
+        int ans = ceil((n/2)-1);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
