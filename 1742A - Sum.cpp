a4cbdad1ccd6af10328ddb2ded4ee8b246f27b68
its-vikash <dev.vikashpatel@gmail.com>
2023-04-12 07:24:26 +0530

    1742A - Sum.cpp

---
+++ 1742A - Sum.cpp
@@ -0,0 +1,23 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a==b+c || b==a+c || c== a+b)
+        cout<<"YES"<<endl;
+        else
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
