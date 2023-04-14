ca843383607fe71d27b0f9c80e963e26ac2bdf19
its-vikash <dev.vikashpatel@gmail.com>
2023-04-14 16:29:57 +0530

    Added 1371A - Magical Sticks.cpp

---
+++ 1371A - Magical Sticks.cpp
@@ -0,0 +1,24 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int ans = 0;
+        ans = n/2;
+        if(n%2)
+        ans++;
+        cout<<ans<<endl;
+    }
+    return 0;
+}
