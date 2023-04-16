091f9c8b76e495340515919348748a2a26993327
its-vikash <dev.vikashpatel@gmail.com>
2023-04-16 09:44:02 +0530

    Added 1283A - Minutes Before the New Year.cpp

---
+++ 1283A - Minutes Before the New Year.cpp
@@ -0,0 +1,20 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int h,m;
+        cin>>h>>m;
+        cout<<(23-h)*60+(60-m)<<endl;
+    }
+    return 0;
+}
