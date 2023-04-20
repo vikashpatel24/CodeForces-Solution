d7de4cceb0a8d3c2678722dc7dd7294737363cfa
its-vikash <dev.vikashpatel@gmail.com>
2023-04-20 18:58:36 +0530

    Added 1520B - Ordinary Numbers.cpp

---
+++ 1520B - Ordinary Numbers.cpp
@@ -0,0 +1,30 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long n, count=0, start;
+    	cin>>n;
+    	for(int i=1; i<=9; i++)
+    	{
+    		start=i;
+    		while(start<=n)
+    		{
+    			count++;
+    			start=start*10+i;
+			}
+		}
+		cout<<count<<endl;
+    }
+    return 0;
+}
+
