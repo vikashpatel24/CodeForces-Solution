12a6dd82e4a85c0d37b05d749e20ac61bae8bb7b
its-vikash <dev.vikashpatel@gmail.com>
2023-05-11 23:37:22 +0530

    Added 1285A - Mezo Playing Zoma.cpp

---
+++ 1285A - Mezo Playing Zoma.cpp
@@ -0,0 +1,17 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+
+int main()
+{
+    int n;
+    string s;
+    cin>>n;
+    cin>>s;
+    cout<<n+1<<endl;
+    return 0;
+}
