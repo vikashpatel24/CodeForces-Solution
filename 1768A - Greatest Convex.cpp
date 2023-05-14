93d989ef304fc7ddc3c83ff8f7998617f0b9b22b
its-vikash <dev.vikashpatel@gmail.com>
2023-05-14 22:12:12 +0530

    Added 1768A - Greatest Convex.cpp

---
+++ 1768A - Greatest Convex.cpp
@@ -0,0 +1,20 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        cout<<n-1<<endl;
+    }
+    return 0;
+}
