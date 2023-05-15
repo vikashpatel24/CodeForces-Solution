290ddf63f912a0ce025c811c9b8c36744de2e1ba
its-vikash <dev.vikashpatel@gmail.com>
2023-05-15 08:38:24 +0530

    Added 1828A - Divisible Array.cpp

---
+++ 1828A - Divisible Array.cpp
@@ -0,0 +1,24 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+    	cin>>n;
+    	for(int i = 1; i <= n; i++) 
+    	{
+    	    cout<<i*2<<" ";
+    	}
+    	cout<<endl;;
+    }
+    return 0;
+}
