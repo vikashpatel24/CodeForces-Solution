3e1b3011960244752229b84ff207c2d950cbcc6a
its-vikash <dev.vikashpatel@gmail.com>
2023-05-28 22:34:53 +0530

    Added 959A - Mahmoud and Ehab and the even-odd game.cpp

---
+++ 959A - Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,18 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+ 
+int main()
+{
+    int n;
+    cin>>n;
+    if(n%2==0)
+    cout<<"Mahmoud"<<endl;
+    else
+    cout<<"Ehab"<<endl;
+    return 0;
+}
