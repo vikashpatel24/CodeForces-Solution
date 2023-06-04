21e8205be6a595408b2f726588112af2423720a1
its-vikash <dev.vikashpatel@gmail.com>
2023-06-04 10:51:17 +0530

    Added 1839A - The Good Array.cpp

---
+++ 1839A - The Good Array.cpp
@@ -0,0 +1,20 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+ 
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+       int n, k;
+       cin>>n>>k;
+       cout<<2+(n-2)/k<<endl;
+    }
+    return 0;
+}
