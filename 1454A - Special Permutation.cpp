0944c20b5d73d1f5740ef4c9354c70a61834828d
its-vikash <dev.vikashpatel@gmail.com>
2023-06-06 17:37:48 +0530

    Added 1454A - Special Permutation.cpp

---
+++ 1454A - Special Permutation.cpp
@@ -0,0 +1,35 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int i = 2;
+        while(i <= n)
+        {
+            cout<<i<<" ";
+            i++;
+        }
+        cout<<1<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
