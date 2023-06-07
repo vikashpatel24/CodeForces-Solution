f8383f0a566003694faf0d013b94e875f99ef5da
its-vikash <dev.vikashpatel@gmail.com>
2023-06-07 10:50:33 +0530

    Added 1840C - Ski Resort.cpp

---
+++ 1840C - Ski Resort.cpp
@@ -0,0 +1,33 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+       int n, k, q;
+       cin>>n>>k>>q;
+       int t[n];
+       int pd = 0;
+       ll ans = 0;
+       for(int i=0; i<n; i++)
+       {
+           cin>>t[i];
+           if(t[i] <= q)
+           pd++;
+           else
+           pd = 0;
+           ans += max(0, pd - k + 1);
+       }
+       cout<<ans<<endl;
+    }
+    return 0;
+}
+
