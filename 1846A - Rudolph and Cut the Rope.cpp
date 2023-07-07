5f4a6353a904ccabb08e532705e86e6768d150f9
its-vikash <dev.vikashpatel@gmail.com>
2023-07-07 20:23:05 +0530

    Added 1846A - Rudolph and Cut the Rope.cpp

---
+++ 1846A - Rudolph and Cut the Rope.cpp
@@ -0,0 +1,44 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("/Users/vikash/Desktop/output.txt", "w", stdout);
+    // #endif
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+       int n;
+       cin>>n;
+       int t = n;
+       int c = 0;
+       int a,b;
+       while(n--)
+       {
+           cin>>a>>b;
+           if(b>=a)
+           c++;
+       }
+       cout<<t-c<<endl;
+    }
+    return 0;
+}
+
