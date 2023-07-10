51b802710045aeb3d670e7a93098c38a7e0a74fd
its-vikash <dev.vikashpatel@gmail.com>
2023-07-10 23:06:56 +0530

    Added 1702A - Round Down the Price.cpp

---
+++ 1702A - Round Down the Price.cpp
@@ -0,0 +1,39 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("/Users/vikash/Desktop/output.txt", "w", stdout);
+    // #endif
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        ll n;
+    	cin >> n;
+    	ll x=1;
+    	while(x*10<=n)
+    	{
+    		x*=10;
+    	}
+    	cout<<n-x<<endl;
+    }
+    return 0;
+}
