4638fe5e7bca0fcf676855d7f1959837cb95a086
its-vikash <dev.vikashpatel@gmail.com>
2023-07-11 23:34:19 +0530

    Added 1626A - Equidistant Letters.cpp

---
+++ 1626A - Equidistant Letters.cpp
@@ -0,0 +1,35 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("/Users/vikash/Desktop/output.txt", "w", stdout);
+    // #endif
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        sort(s.begin(), s.end());
+        cout<<s<<endl;
+    }
+    return 0;
+}
