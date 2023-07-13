e5eaa7b3f96556756a7371e13a301622074fab6d
its-vikash <dev.vikashpatel@gmail.com>
2023-07-13 22:18:42 +0530

    Added 1660A - Vasya and Coins.cpp

---
+++ 1660A - Vasya and Coins.cpp
@@ -0,0 +1,42 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+
+int solve(int a, int b)
+{
+    if(a == 0)
+    return 1;
+    else
+    return a*1+b*2+1;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("/Users/vikash/Desktop/output.txt", "w", stdout);
+    // #endif
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a, b;
+        cin>>a>>b;
+        cout<<solve(a, b)<<endl;
+    }
+    return 0;
+}
