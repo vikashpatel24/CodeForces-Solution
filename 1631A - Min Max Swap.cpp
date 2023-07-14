e22ac8dbdc1071c9ec7858928db576e2c997d523
its-vikash <dev.vikashpatel@gmail.com>
2023-07-14 23:55:40 +0530

    Added 1631A - Min Max Swap.cpp

---
+++ 1631A - Min Max Swap.cpp
@@ -0,0 +1,49 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("/Users/vikash/Desktop/output.txt", "w", stdout);
+    // #endif
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+    cin>>n;
+    int arr[n],brr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        cin>>brr[i];
+        if(arr[i]>brr[i])
+        {
+            swap(arr[i],brr[i]);
+        }
+    }
+    sort(arr,arr+n);
+    sort(brr,brr+n);
+    cout<<arr[n-1]*brr[n-1]<<endl;
+    }
+    return 0;
+}
