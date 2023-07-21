692e31bed1e1336917d515e60d42dde96e68e8e2
its-vikash <dev.vikashpatel@gmail.com>
2023-07-21 18:51:16 +0530

    Added 1399A - Remove Smallest.cpp

--- 1399A - Remove Smallest.cpp
+++ 1399A - Remove Smallest.cpp
@@ -1,40 +1,54 @@
+//                                 ॐ नमः शिवाय
 #include<bits/stdc++.h>
 using namespace std;
 
-//Code Written By: Vikash Patel
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
 
-#define ll              long long
-#define ull             unsigned long long
-#define pb              push_back
-#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+string solve(int a[], int n)
+{
+    if(n == 1)
+    return "YES";
+    sort(a, a+n);
+    int maxi = INT_MIN;
+    for(int i=1; i<n; i++)
+    {
+        maxi = max(maxi, a[i]-a[i-1]);
+    }
+    if(maxi > 1)
+    return "NO";
+    return "YES";
+}
 
 int main()
 {
-    fastread();
-    int t,n,a[55];
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("/Users/vikash/Desktop/output.txt", "w", stdout);
+    // #endif
+    
+    int t;
     cin>>t;
-    while(t--){
+    while(t--)
+    {
+        int n;
         cin>>n;
-        for(int i=0; i<n; i++){
+        int a[n];
+        for(int i=0; i<n; i++)
+        {
             cin>>a[i];
         }
-        if(n == 1){
-            cout<<"YES"<<endl;
-            continue;
-        }
-        sort(a,a+n);
-        vector<int>v;
-        int dif = 0;
-        for(int i=1; i<n; i++){
-            dif = abs(a[i] - a[i-1]);
-            v.push_back(dif);
-        }
-        sort(v.begin(), v.end(), greater<int>());
-        if(v[0] > 1){
-            cout<<"NO"<<endl;
-        }
-        else{
-            cout<<"YES"<<endl;
-        }
+        cout<<solve(a, n)<<endl;
     }
-} 
+    return 0;
+}
