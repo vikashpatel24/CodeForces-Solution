ea44d0d599486e64916d082459c0fff965f9111c
its-vikash <dev.vikashpatel@gmail.com>
2023-07-21 21:17:43 +0530

    Added 1850A - To My Critics.cpp

---
+++ 1850A - To My Critics.cpp
@@ -0,0 +1,37 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("/Users/vikash/Desktop/output.txt", "w", stdout);
+    // #endif
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a, b, c;
+        cin>>a>>b>>c;
+        if(a+b >= 10 || a+c >= 10 || b+c>= 10)
+        cout<<"YES"<<endl;
+        else
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
