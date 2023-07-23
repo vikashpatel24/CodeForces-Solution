fd2aee201179aa860b752d53dbfd9eff90c5e998
its-vikash <dev.vikashpatel@gmail.com>
2023-07-23 23:44:46 +0530

    Added 265A - Colorful Stones (Simplified Edition).cpp

---
+++ 265A - Colorful Stones (Simplified Edition).cpp
@@ -0,0 +1,36 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("/Users/vikash/Desktop/output.txt", "w", stdout);
+    // #endif
+    
+    string s, t;
+    cin>>s>>t;
+    int liss = 1;
+    s = "0" + s;
+    for (int i = 0; i < t.size(); i++) 
+    {
+        if (t[i] == s[liss]) 
+        liss++;
+    }
+    cout<<liss<<endl;
+    return 0;
+}
