2f076b6c8e80b806cb73d33e1902a4af8a4cf65f
its-vikash <dev.vikashpatel@gmail.com>
2023-07-27 14:28:55 +0530

    Added 1353A - Most Unstable Array.cpp

---
+++ 1353A - Most Unstable Array.cpp
@@ -0,0 +1,43 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+
+int solve(int n, int m)
+{
+    if(n == 1)
+    return 0;
+    if(n == 2)
+    return m;
+    return m*2;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("/Users/vikash/Desktop/output.txt", "w", stdout);
+    // #endif
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, m;
+        cin>>n>>m;
+        cout<<solve(n, m)<<endl;
+    }
+    return 0;
+}
