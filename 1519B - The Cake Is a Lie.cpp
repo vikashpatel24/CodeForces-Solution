7e6d08c74e20852d7ad3fd0869f41ccfbe250f8e
its-vikash <dev.vikashpatel@gmail.com>
2023-08-05 14:25:38 +0530

    Added 1519B - The Cake Is a Lie.cpp

---
+++ 1519B - The Cake Is a Lie.cpp
@@ -0,0 +1,41 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+
+string solve()
+{
+    int n, m, k;
+    cin>>n>>m>>k;
+    if(n*m-1 == k)
+    return "YES";
+    return "NO";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("/Users/vikash/Desktop/output.txt", "w", stdout);
+    // #endif
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        cout<<solve()<<endl;
+    }
+    return 0;
+}
