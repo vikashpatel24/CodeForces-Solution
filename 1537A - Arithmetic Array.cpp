6e6b42ccaf5c5e88df3ff348b2a3abc0a1a89698
its-vikash <dev.vikashpatel@gmail.com>
2023-08-17 23:40:31 +0530

    Added 1537A - Arithmetic Array.cpp

---
+++ 1537A - Arithmetic Array.cpp
@@ -0,0 +1,48 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+
+int solve()
+{
+    int n; 
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++)
+    cin>>a[i];
+    int sum = accumulate(a, a+n, 0);
+    if(sum - n == 0)
+    return 0;
+    if(sum <= 0 || sum < n)
+    return 1;
+    else
+    return sum-n;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("/Users/vikash/Desktop/output.txt", "w", stdout);
+    // #endif
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        cout<<solve()<<endl;
+    }
+    return 0;
+}
