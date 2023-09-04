7d78c1838384deb33f8dee568fb4435fcfeab394
its-vikash <dev.vikashpatel@gmail.com>
2023-09-04 23:50:39 +0530

    Added 1519A - Red and Blue Beans.cpp

---
+++ 1519A - Red and Blue Beans.cpp
@@ -0,0 +1,35 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("/Users/vikash/Desktop/output.txt", "w", stdout);
+    // #endif
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int r, b, d;
+        cin>>r>>b>>d;
+		if(r<b)swap(r,b);
+		cout<<((r+b-1)/b-1<=d?"YES":"NO")<<endl;
+    }
+    return 0;
+}
