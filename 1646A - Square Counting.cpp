c0eebd408ea2f071f96320df7f321ca748cdd19d
its-vikash <dev.vikashpatel@gmail.com>
2023-09-10 20:38:35 +0530

    Added 1646A - Square Counting.cpp

---
+++ 1646A - Square Counting.cpp
@@ -0,0 +1,45 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+
+ll solve()
+{
+    ll n, s;
+    cin>>n>>s;
+    if(s == 0)
+    return 0;
+    ll sq = n*n;
+    if(s<sq)
+    return 0;
+    else
+    return s/sq;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("/Users/vikash/Desktop/output.txt", "w", stdout);
+    // #endif
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        cout<<solve()<<endl;
+    }
+    return 0;
+}
