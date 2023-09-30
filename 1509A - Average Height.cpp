85c0c238b2635aef70d9ed97b7bd990c1ed8ef2e
its-vikash <dev.vikashpatel@gmail.com>
2023-09-30 20:02:50 +0530

    Added 1509A - Average Height.cpp

---
+++ 1509A - Average Height.cpp
@@ -0,0 +1,52 @@
+//                                 ॐ नमः शिवाय
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+ 
+// Code Written By: Vikash Patel
+// Codeforces Profile: https://codeforces.com/profile/vikashpatel
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("/Users/vikash/Desktop/output.txt", "w", stdout);
+    // #endif
+    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int h[n];
+        vector<int> odd, even;
+        for(int i=0;i<n; i++)
+        {
+            cin>>h[i];
+            if(h[i]%2)
+            odd.push_back(h[i]);
+            else
+            even.push_back(h[i]);
+        }
+        for(auto i : odd)
+        {
+            cout<<i<<" ";
+        }
+        for(auto i : even)
+        {
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
